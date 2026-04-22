ab9a777625f160ff761cfe690dfe53060fc4a143
pramodkumarPradhan <pramodpradhan369@outlook.com>
2026-04-22 17:56:41 +0530

    may be fixed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,11 @@
-const config = {
+export default {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-export default config;
